6685750a4405c29d2edbde479b629a8f4d96d54b
Natália Azevedo <naaatalia.azevedo@live.com>
2019-02-17 20:20:02 -0300

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+let defaultTextColor = '#fff';
+let defaultStyle = {
+  color: defaultTextColor,
+};
+class Aggregate extends Component {
+  render() {
+    return(
+      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style={{ ...defaultStyle }}>
+        <img src="" />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+      <div style={{ ...defaultStyle, width: '25%', display: 'inline-block' }}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 1</li>
+          <li>Song 1</li>
+          <li>Song 1</li>
+          <li>Song 1</li>
+          <li>Song 1</li>
+        </ul>
+      </div>
+    );
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
       </div>
     );
   }
